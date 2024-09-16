8ef9fa725d5effb0c49807408b3541cfd013da00
Ajay Raj Tiwari <ajay.tiwari@sociolla.com>
2024-09-16 21:57:39 +0530

    Added url

--- ConnectMe/src/services/config/default.env.js
+++ ConnectMe/src/services/config/default.env.js
@@ -1,8 +1,10 @@
 "use strict";
 let MS_MY_BUDDY_PUBLIC;
-const ENV = "local";
+const ENV = "uat";
 if (ENV === "local") {
-  MS_MY_BUDDY_PUBLIC = `http://192.168.17.110:3000`;
+  MS_MY_BUDDY_PUBLIC = `http://192.168.8.101:3000`;
+} else {
+  MS_MY_BUDDY_PUBLIC = `https://findmybuddy-api.onrender.com`;
 }
 export default {
   NODE_ENV: "development",
